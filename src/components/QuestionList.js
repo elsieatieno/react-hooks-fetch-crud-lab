895e8971b89413800de3df6b5ff8bf7d5961882f
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-07-26 15:14:33 +0300

    Completed assignment

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -10,3 +10,34 @@ function QuestionList() {
 }
 
 export default QuestionList;
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
+
+function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((res) => res.json())
+      .then((data) => setQuestions(data));
+  }, []);
+
+  function onDelete(selectedQuestion) {
+    setQuestions((questions) =>
+      questions.filter((question) => question.id !== selectedQuestion.id)
+    );
+  }
+
+  return (
+    <section>
+      <h1>Quiz Questions</h1>
+      <ul>
+        {questions.map((q) => (
+          <QuestionItem question={q} onDelete={onDelete} />
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default QuestionList;
