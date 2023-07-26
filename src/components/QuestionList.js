9fbcb501b290b7cfff029943b203e153ee45d756
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-07-26 15:15:18 +0300

    Completed assignment

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,15 +1,4 @@
-import React from "react";
 
-function QuestionList() {
-  return (
-    <section>
-      <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
-  );
-}
-
-export default QuestionList;
 import React, { useEffect, useState } from "react";
 import QuestionItem from "./QuestionItem";
 
